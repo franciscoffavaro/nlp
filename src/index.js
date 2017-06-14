8859ca7e5ccbeff1f89669d64352bae4476fd3a6
Francisco Favaro <franciscoffavaro@gmail.com>
2017-06-14 07:15:17 -0300

    Correção de Métodos

--- src/index.js
+++ src/index.js
@@ -13,25 +13,27 @@ const treinamento = fs.readFileSync('shakespeare.txt', 'utf8').split(' ').filter
 //eu corri muito
 
 const findWordAfter = (palavra, arrayTreinamento) => {
-  let words = [];
-  for (let i=0; i<arrayTreinamento.lenght; i++){
-    if(arrayTreinamento[i] == palavra){
-      console(arrayTreinamento[i]);
-      for(let j=0; j<words.lenght; j++){
-        if(words[j].word == palavra){
-          words[j].count +=1;
-        }else{
-          words.push({
-            word: palavra,
-            count: 1
-          })
-        }
-      }
-    }
-  }
-  return words;
-}
+//   let words = [];
+//   for (let i=0; i<arrayTreinamento.length; i++){
+//     if(arrayTreinamento[i].toUpperCase() === palavra.toUpperCase()){
+//       console.log(arrayTreinamento[i]);
+//       for(let j=0; j<words.length; j++){
+//         if(words[j].word == palavra){
+//           words[j].count +=1;
+//         }else{
+//
+//           words.push({
+//             word: palavra,
+//             count: 1
+//           })
+//         }
+//       }
+//     }
+//   }
+//   return words;
+// }
 
-console.log(findWordAfter('muito', treinamento));
+console.log(findWordAfter('now', treinamento));
+
 //Leitura dos parâmetros no Temrinal.
 const params = process.argv.slice(2);
